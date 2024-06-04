681206350f3166630f778149812a95a93a8c2d56
AzkaZharari <azka22060ti@student.nurulfikri.ac.id>
2024-06-04 22:08:22 +0700

    Praktikum Per-9

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,36 @@
 import styles from './Hero.module.css';
+import { useState, useEffect } from 'react';
 
 function Hero() {
+
+    const [movie, setMovie] = useState("");
+
+    useEffect(() => {
+        async function fetchMovie() {
+            const url= "https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590";
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            setMovie(data);
+        }
+        fetchMovie();
+    } , []);
+
     return (
         <div className={styles.container}>
             <section className={styles.hero}>
                 <div className={styles.hero__left}>
-                    <h1 className={styles.hero__title}>Dune: Part Two</h1>
-                    <h3 className={styles.hero__genre}>Genre : Adventure, Action, Drama</h3>
-                    <p className={styles.hero__description}>
-                        Paul Atreides unites with Chani and the Fremen while on a warpath of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, he endeavors to prevent a terrible future only he can foresee
+                    <h1 className={styles.hero__title}>{movie.Title}</h1>
+                    <h3 className={styles.hero__genre}>Genre : {movie.Genre}</h3>
+                    <p className={styles.hero__description}>{movie.Plot}
                     </p>
                     <button className={styles.hero__button}>Watch</button>
                 </div>
                 <div className={styles.hero__right}>
                     <img
                     className={styles.hero__image} 
-                    src="https://www.tajukflores.com/assets/img/rmqiV9OperdByzpmCbhQ5DrImXu-800x450.jpg" alt="placeholder" />
+                    src={movie.Poster} alt={movie.Title} />
                 </div>
             </section>
         </div>
@@ -23,3 +38,4 @@ function Hero() {
 }
 
 export default Hero;
+
